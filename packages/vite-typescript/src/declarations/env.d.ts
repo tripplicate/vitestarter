9dd01f3fbb160eab2993f4677af471079950446d
Danil Sergeev <tripplicate.me@gmail.com>
2023-05-19 19:26:27 +0400

    chore(workspace,deps): update dependencies

--- packages/vite-typescript/src/declarations/env.d.ts
+++
@@ -1,7 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_APP: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- packages/vite-typescript/vite/plugins/vite-tsconfig-paths.ts
+++
@@ -1,5 +0,0 @@
-import type { PluginOptions } from 'vite-tsconfig-paths'
-
-export const ViteTsconfigPathsConfig: PluginOptions = {
-  loose: false,
-}
