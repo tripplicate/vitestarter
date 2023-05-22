ceda37e26d600d41f1a4c5b8225353d2b501b3b6
Danil Sergeev <tripplicate.me@gmail.com>
2023-05-22 18:07:04 +0400

    chore(template): add `javascript-vanilla` template

---
+++ packages/template/javascript/vanilla/src/index.js
@@ -0,0 +1,1 @@
+export const simpleFn = () => 0;
---
+++ packages/template/javascript/vanilla/vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+import defaultConfig from './vite';
+
+export default defineConfig(options => defaultConfig(options));
