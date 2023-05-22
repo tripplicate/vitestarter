d0fa58ed3221631c76169ee793b75bc47bd64899
Danil Sergeev <tripplicate.me@gmail.com>
2023-05-22 15:36:34 +0400

    chore(deps): update dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['@trpl/javascript', '@trpl/documents'],
   rules: {
     'no-console': ['off'],
-    'unicorn/filename-case': ['off'],
     'import/exports-last': ['off'],
     'import/extensions': ['off'],
   },
